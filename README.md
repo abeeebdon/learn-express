863ffbaad5b2f79c7571bf29880e14826ebc6a1a
abeebdon <abeebdon@gmail.com>
2024-12-26 23:25:37 +0100

    Middleware in express js

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # learn-express
+
+### Things u can get from request
+
+# params or query
+
+# body
+
+##
+
+http://localhost:3100/api/users?file=name&value=ab
--- src/index.mjs
+++ src/index.mjs
@@ -2,8 +2,9 @@ import express, { response } from 'express'
 
 const app = express()
 
+app.use(express.json())
 //run at this port
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3100
 
 //user array
 const userArray = [
@@ -26,14 +27,15 @@ app.get('/', (request, response) => {
 
 app.get('/api/users', (request, response) => {
   const {
-    query: { filter, value },
+    query: { file, value },
   } = request
+  // you can destructure query parameters from a request it is important when you search a list from the backend
 
-  if (!filter || !value) return response.status(200).send(userArray)
+  if (!file || !value) return response.status(200).send(userArray)
 
   //if there is filter and value
   const filteredData = userArray.filter((user) =>
-    user[filter].toLowerCase().includes(value.toLowerCase())
+    user[file].toLowerCase().includes(value.toLowerCase())
   )
   return response.status(200).send(filteredData)
 })
@@ -46,6 +48,42 @@ app.get('/api/users/:id', (request, response) => {
   return response.send(findUser)
 })
 
+app.post('/api/users', (request, response) => {
+  const { body } = request
+
+  // You can also destructure body that is passed in from request hence
+  // query
+  // body
+
+  const newUser = { id: userArray.length, ...body }
+  userArray.push(newUser)
+  return response.status(201).send(newUser)
+})
+app.put('/api/users/:id', (request, response) => {
+  const {
+    body,
+    params: { id },
+  } = request
+
+  const parsedId = parseInt(id)
+  if (isNaN(parsedId)) return response.sendStatus(400)
+  const findUserIndex = userArray.findIndex((user) => user.id === parsedId)
+  // console.log(findUserIndex)
+  if (findUserIndex === -1) return response.sendStatus(404)
+  userArray[findUserIndex] = { id: parsedId, ...body }
+  return response.send(userArray)
+})
+
+app.post('/abeeb', (req, res) => {
+  const { body } = req
+  console.log(body)
+  if (!body) res.sendStatus(400)
+  const newPost = { id: userArray.length + 1, ...body }
+  console.log(newPost)
+
+  userArray.push(newPost)
+  return res.send(userArray)
+})
 app.listen(PORT, () => {
   console.log(`Listening at ${PORT}`)
 })
